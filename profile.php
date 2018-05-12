cc748b7e9bca3ef7734103f1ed3c17fbf140e55b
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-12 16:13:06 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,14 +1,9 @@
 <?php
 
+$url = 'https://api.line.me/v2/bot/profile/U126269d84ad11b29dd3be5979c1be03f';
 
-$access_token = 'ff/Y7DOQEFQL3M8Aow20tzs576rDp4PALLb3gEqDFr2AK1TTEen3eNBmWrmy3Zz9wT+spR/vnrbSEhRzP79EzXZBPVww+DMn6AWMAz4rEQ1EdqxJHrfaQ/RzeKXyRb9RM4BZOlokBAVrlDvzRK17IgdB04t89/1O/w1cDnyilFU=
-';
-
-$userId = 'U126269d84ad11b29dd3be5979c1be03f';
-
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
-
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ff/Y7DOQEFQL3M8Aow20tzs576rDp4PALLb3gEqDFr2AK1TTEen3eNBmWrmy3Zz9wT+spR/vnrbSEhRzP79EzXZBPVww+DMn6AWMAz4rEQ1EdqxJHrfaQ/RzeKXyRb9RM4BZOlokBAVrlDvzRK17IgdB04t89/1O/w1cDnyilFU=
+');
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
