d588299c416cfe9caf12057a053332e09ea944a2
sevendays2018 <39212577+sevendays2018@users.noreply.github.com>
2018-05-12 17:32:17 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,22 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['text'] == '\report') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			
+			$url = 'https://api.line.me/v2/bot/profile/' . $event['source']['userId'];
+
+$headers = array('Authorization: Bearer ff/Y7DOQEFQL3M8Aow20tzs576rDp4PALLb3gEqDFr2AK1TTEen3eNBmWrmy3Zz9wT+spR/vnrbSEhRzP79EzXZBPVww+DMn6AWMAz4rEQ1EdqxJHrfaQ/RzeKXyRb9RM4BZOlokBAVrlDvzRK17IgdB04t89/1O/w1cDnyilFU=
+');
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$finalreport = curl_exec($ch);
+curl_close($ch);
+
+			
+			//$text = $event['source']['userId'];
+			$text = $finalreport;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
